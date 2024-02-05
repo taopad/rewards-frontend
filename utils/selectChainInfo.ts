9e5ff37fdf2b7f6a18cd82e6062f8a6c51ba8825
niera26 <niera26@outlook.com>
2024-02-05 16:38:30 +0100

    remove testnet and dont throw in selectChain

--- utils/selectChainInfo.ts
+++ utils/selectChainInfo.ts
@@ -4,7 +4,7 @@ export const selectChainInfo = (chainId: number) => {
     const chain = chains.filter(chain => chain.id === chainId).shift()
 
     if (chain === undefined) {
-        throw new Error("non supported chain id")
+        return { chain: { name: "-" }, logo: "" }
     }
 
     const logo = chain.name.toLowerCase().replace(" ", "-")
