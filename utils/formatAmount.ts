362e58ab17423dab1d9f966f5340cff1861e2b61
niera26 <niera26@outlook.com>
2024-02-26 16:58:26 +0100

    display numbers with US locale formatting ans display up to 5 fractional numbers

--- utils/formatAmount.ts
+++ utils/formatAmount.ts
@@ -1,5 +1,7 @@
 import { formatUnits } from "viem"
 
 export const formatAmount = (amount: bigint, decimals: number) => {
-    return parseFloat(formatUnits(amount, decimals)).toLocaleString()
+    return parseFloat(formatUnits(amount, decimals)).toLocaleString("en-US", {
+        maximumFractionDigits: 5,
+    })
 }
