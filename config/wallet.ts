1fc02e8cc2d3935f44c32040ac01c88ff70a777b
niera26 <niera26@outlook.com>
2024-03-03 15:00:40 +0100

    remove ethereum

--- config/wallet.ts
+++ config/wallet.ts
@@ -1,4 +1,4 @@
-import { mainnet, arbitrum } from "wagmi/chains"
+import { arbitrum } from "wagmi/chains"
 import { createConfig, createStorage, cookieStorage, http, fallback } from "wagmi"
 import { connectorsForWallets } from "@rainbow-me/rainbowkit"
 import {
@@ -14,7 +14,6 @@ const appName = "Taopad app"
 const projectId = "031d4ad6ce63b830ab346fb92b96f328"
 
 const rpcs = {
-    [mainnet.id]: "https://rpc.ankr.com/eth",
     [arbitrum.id]: "https://rpc.ankr.com/arbitrum",
 }
 
@@ -41,9 +40,8 @@ export const config = createConfig({
     ssr: true,
     storage,
     connectors,
-    chains: [mainnet, arbitrum],
+    chains: [arbitrum],
     transports: {
-        [mainnet.id]: fallback([http(rpcs[mainnet.id]), http()]),
         [arbitrum.id]: fallback([http(rpcs[arbitrum.id]), http()]),
     },
 })
